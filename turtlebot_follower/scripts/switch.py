9531470e4d18fbdd7f63a30e1a056f6d99a2b0f5
xiaoqiang <xiaoqiang@bluewhale.com>
2017-03-20 08:26:33 +0800

    add set_tilt_degree publisher

--- turtlebot_follower/scripts/switch.py
+++ turtlebot_follower/scripts/switch.py
@@ -3,7 +3,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-
+from std_msgs.msg import Int16
 # This script will listen for joystick button 5 being toggled and
 # send zero speed messages to the mux to disable the follower until
 # button 5 is pressed again.
@@ -23,8 +23,12 @@ class BehaviorSwitch(object):
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         pub = rospy.Publisher('cmd_vel_mux/input/switch', Twist, queue_size=10)
         rospy.Subscriber('joy', Joy, self.callback)
+        tilt_pub = rospy.Publisher('/set_tilt_degree', Int16 , queue_size=1)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
+            tilt_degree=Int16()
+            tilt_degree.data=0
+            tilt_pub.publish(tilt_degree)
             if self.running:
                 empty_msg = Twist()
                 pub.publish(empty_msg)
